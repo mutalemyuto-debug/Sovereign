a1ec8e92b484434be37fccae5ef3776b5daba686
mutalemyuto-debug <mutalemyuto@gmail.com>
2026-09-13 12:37:17 -0700

    Fix media serving when debug is disabled

--- sovereign/urls.py
+++ sovereign/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.static import serve
 
 from sovereign.core import views
 
@@ -25,4 +25,10 @@ urlpatterns = [
     path("profile/", views.update_profile, name="profile"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path(
+        "media/<path:path>",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
